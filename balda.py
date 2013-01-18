099f02fae01138c05dfd846021aa0425ebe3f410
IKV <kx13@ya.ru>
2013-01-18 09:48:58 +0400

    добавлен список использованых слов и можно просмативать все варианты слов

--- balda.py
+++ balda.py
@@ -16,6 +16,14 @@ def CleanBackground():
         for j in range(5):
             rows[i][j].config(bg='white')     
 
+def AddToUsedList(word):
+    usedwords.append(word)
+    usedlist.insert('end',word)
+
+def AddToUsed():
+    used=askstring("Использованное слово","Введите слово")
+    AddToUsedList(used)
+
 def NewWord():
     newword=askstring("Новое слово","Введите слово")
     if len(newword)<3:
@@ -31,18 +39,24 @@ def NewGame():
     usedwords=[]
     listcell=[]
     firstword=askstring("Начало игры","Введите слово") #"слово" #
-    usedwords.append(firstword)
     if len(firstword)!=5:
         showerror("ошибка!","длина слова должна быть 5 символов")
         return
     CleanBoard()
     CleanBackground()
+    if usedlist.size():
+        usedlist.delete(0,usedlist.size())
     for i in range(5):
        rows[2][i].insert(END,firstword[i])
     findwords.config(state=NORMAL)
+    AddToUsedList(firstword)
 
 def FindWords():
     global listcell
+    index = words.curselection() 
+    if index:
+        label = words.get(index)  
+        AddToUsedList(label)
     if words.size():
         words.delete(0,words.size())
     cellrow=[]
@@ -58,19 +72,19 @@ def FindWords():
         c.MakeNodes(cellrow)
     listcell=[]
     findedwords=sorted(logic.FindWords(cells),key=lambda item: len(item[0]),reverse=True)
-
     for w in findedwords:     
         if w[0] not in usedwords:
             words.insert('end',w[0])
             listcell.append((w[1],w[2],w[3]))
     
     
-
 def ListClicked(event):
+    global newletterpos
+    if newletterpos:
+        newletterpos.delete(0,END)
     index = words.curselection() 
     if index:
         CleanBackground()
-        label = words.get(index)  
         item=listcell[int(index[0])]
         for i in item[0]:
             rows[i.row][i.column].config(bg='yellow')
@@ -78,12 +92,12 @@ def ListClicked(event):
         newletterpos.delete(0,END)
         newletterpos.insert(END,item[1])
         newletterpos.config(bg='cyan')
-        usedwords.append(label)
 
 
 rows = []
 usedwords=[]
 listcell=[]
+newletterpos=None
 
 root = Tk()
 board = Frame(root)
@@ -98,13 +112,17 @@ for i in range(5):
 newword = Button(root, text='Добавить в словарь',command=NewWord)
 newgame = Button(root, text='Новая игра',command=NewGame)
 findwords = Button(root, text='Найти слова',command=FindWords,state=DISABLED)
-words =  Listbox(root, height=20,selectmode=SINGLE)
+words =  Listbox(root, height=10,selectmode=SINGLE)
+usedlist =  Listbox(root, height=20,selectmode=SINGLE)
+addtoused = Button(root, text='Добавить в использованные',command=AddToUsed)
 words.bind('<Double-1>', ListClicked) 
 newword.pack(pady=5)
 newgame.pack(pady=10)
 board.pack()
 findwords.pack(pady=3)
 words.pack()
+usedlist.pack(pady=3)
+addtoused.pack()
 
 logic.Init()
 mainloop()
